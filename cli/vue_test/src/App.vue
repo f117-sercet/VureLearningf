791ff03188cd88112840930ac73da285396df057
sowrd <15159885771@163.com>
2024-02-17 20:50:29 +0800

    ref属性的使用

--- cli/vue_test/src/App.vue
+++ cli/vue_test/src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 v-text="msg" ref="title"></h1>
+    <button @click="showDom">点我展示dom</button>
+    <HelloWorld msg="Welcome to Your Vue.js App" ref="sch"/>
   </div>
 </template>
 
+<!-------
+  ref 属性
+  1.被用来给元素或子组件注册引用信息
+  2.应用在html标签上获取的是真实Dom元素，应用在组件标签上是组件实例对象
+
+
+---------->
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
@@ -12,6 +21,17 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      msg:'你好'
+    }
+  },
+  methods:{
+    showDom(){
+     this.$refs.title // 真实dom元素
+      this.$refs.sch // 组件VC
+    }
   }
 }
 </script>
